756c61337181bd979008ee9b55005382871f8d0d
Khubaib Husan <hafizmkh45@gmail.com>
2025-05-23 23:02:29 +0500

    Update selectionSort.java

--- selectionSort.java
+++ selectionSort.java
@@ -9,6 +9,7 @@ public class SelectionSort {
 
     public static void main(String[] args) {
         int[] arr = {7, 8, 3, 1, 2};
+        // Time Complexity=O(n^2)
 //        Outer Loop: Iterates through each index of the array.
         for (int i = 0; i < arr.length - 1; i++) {
             int smallest = i;
